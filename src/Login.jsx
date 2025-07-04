a4f5f3186215e577c07b41d517fd2c19873bc978
Mahesh131005 <137376494+Mahesh131005@users.noreply.github.com>
2025-07-04 17:00:16 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}auth/login`, {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
         email,
         password,
       });
